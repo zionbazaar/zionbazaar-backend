92bad6134b4cb0b3c9be36dc8a3fbb2643a1a667
zionbazaar <ziontechie.care@gmail.com>
2026-04-15 16:08:30 +0530

    Add files via upload

--- models/Product.js
+++ models/Product.js
@@ -25,6 +25,11 @@ const productSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now }
   }],
   tags: [String],
+  specialBadge: { 
+    type: String, 
+    enum: ['', 'Trending Now', 'Best Seller', 'Limited Offer'],
+    default: '' 
+  },
   isFeatured: { type: Boolean, default: false },
   isActive: { type: Boolean, default: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
